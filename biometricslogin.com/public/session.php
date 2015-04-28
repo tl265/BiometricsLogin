f030fba505cf46a611b0ed67ac95102cfe59ce4c
tl265 <tl265@cornell.edu>
2015-04-28 11:56:51 -0400

    now sleep will be terminated if browser is closed.

--- biometricslogin.com/public/session.php
+++ biometricslogin.com/public/session.php
@@ -1,4 +1,5 @@
 <?php
+	ignore_user_abort(FALSE);
 	
 	$connection = mysql_connect("localhost", "root", "");// Establishing Connection with Server 
 	$db = mysql_select_db("company", $connection);// Selecting Database
@@ -10,11 +11,17 @@
 	$maxiter=30;
 	
 	do{	// wait until the timestamp has been acknowledged by an iphone and posted back to the database
+		if (connection_status() != CONNECTION_NORMAL){
+			$count = $maxiter;
+			break;
+		}	
 		sleep(2);
 		$query = mysql_query("select * from token where timestamp='$timestamp_check'", $connection);
 		$rows = mysql_num_rows($query);
 		$count = $count + 1;
-		print " ";
+		print ' ';
+		ob_flush();
+		flush();
 	} while (($rows < 1) and ($count < $maxiter));
 
 	if ($count == $maxiter)
